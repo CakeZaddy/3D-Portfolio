20db917aa0b51fcf94cb7bcbcf5f50e4cb8036f5
Sulaimon Omotosho <susudef2u@gmail.com>
2023-05-02 09:46:55 +0100

    Contacts, EarthCanvass, Stars, finished Works

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { styles } from '../styles'
@@ -8,6 +8,21 @@ import { menu, close } from '../assets'
 const Navbar = () => {
   const [active, setActive] = useState('')
   const [toggle, setToggle] = useState(false)
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY
+      if (scrollTop > 100) {setScrolled(true)}
+      else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => removeEventListener('scroll', handleScroll)
+  }, [])
 
   return (
     <nav
