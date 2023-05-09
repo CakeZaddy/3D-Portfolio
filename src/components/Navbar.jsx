f4f347b1601a791064589700b9ece56cd039a279
Sulaimon Omotosho <susudef2u@gmail.com>
2023-05-09 13:11:44 +0100

    again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,9 @@ const Navbar = () => {
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY
-      if (scrollTop > 100) {setScrolled(true)}
-      else {
+      if (scrollTop > 100) {
+        setScrolled(true)
+      } else {
         setScrolled(false)
       }
     }
@@ -26,26 +27,33 @@ const Navbar = () => {
 
   return (
     <nav
-    className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
-        to='/'
-        className='flex items-center gap-2'
-        onClick={() => {
-          setActive('')
-          window.scrollTo(0, 0)
-        }} >
-          <img src='https://res.cloudinary.com/dwgberjxq/image/upload/v1682444695/3D/logo_white_transparent_spkbwb.svg' alt="logo" className='w-10 h-12 object-contain' />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex ">Uncle Sula &nbsp; <span className='sm:block hidden' >| Tech Bro</span> </p>
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive('')
+            window.scrollTo(0, 0)
+          }}
+        >
+          <img
+            src='https://res.cloudinary.com/dwgberjxq/image/upload/v1682444695/3D/logo_white_transparent_spkbwb.svg'
+            alt='logo'
+            className='w-10 h-12 object-contain'
+          />
+          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+            Uncle Sula &nbsp;{' '}
+            <span className='sm:block hidden'>| Tech Bro</span>{' '}
+          </p>
         </Link>
-        <ul className="list-none hidden sm:flex  flex-row gap-10">
+        <ul className='list-none hidden sm:flex  flex-row gap-10'>
           {navLinks.map((link) => (
             <li
               key={link.id}
               className={`${
-                active === link.title
-                  ? 'text-white'
-                  : 'text-secondary'
+                active === link.title ? 'text-white' : 'text-secondary'
               } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(link.title)}
             >
@@ -54,31 +62,33 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img
-          src={toggle ? close : menu}
-          alt="menu"
-          className='w-[28px] h-[28px] object-contain cursor-pointer'
-          onClick={() => setToggle(!toggle)}
-           />
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
 
-           <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-           <ul className="list-none flex justify-end items-start flex-col gap-4">
-          {navLinks.map((link) => (
-            <li
-              key={link.id}
-              className={`${
-                active === link.title
-                  ? 'text-white'
-                  : 'text-secondary'
-              } font-poppins font-medium cursor-pointer text-[16px]`}
-              onClick={() => setActive(link.title)}
-            >
-              <a href={`${link.id}`}>{link.title}</a>
-            </li>
-          ))}
-        </ul>
-           </div>
+          <div
+            className={`${
+              !toggle ? 'hidden' : 'flex'
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? 'text-white' : 'text-secondary'
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => setActive(link.title)}
+                >
+                  <a href={`${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
