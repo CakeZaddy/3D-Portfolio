a9c2cb8437098e2ce0fa4de46a9c73ec38336bce
Sulaimon Omotosho <susudef2u@gmail.com>
2023-05-09 13:49:24 +0100

    removed skip tag from all pages except hero

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -57,24 +57,6 @@ const Feedbacks = () => {
           <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
         ))}
       </div>
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#contact'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.dev
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: 'loop',
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
     </div>
   )
 }
