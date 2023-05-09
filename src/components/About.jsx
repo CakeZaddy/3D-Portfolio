da739a2a612dbb476abe36857485548fb64ac8c8
Sulaimon Omotosho <susudef2u@gmail.com>
2023-05-09 13:35:50 +0100

    refurbishment

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -55,6 +55,24 @@ const About = () => {
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#work'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </>
   )
 }
--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -6,36 +6,47 @@ import { SectionWrapper } from '../hoc'
 import { fadeIn, textVariant } from '../utils/motion'
 import { testimonials } from '../constants'
 
-const FeedbackCard = ({ index, testimonial, name, designation, company, image}) => (
+const FeedbackCard = ({
+  index,
+  testimonial,
+  name,
+  designation,
+  company,
+  image,
+}) => (
   <motion.div
     variants={fadeIn('', 'spring', index * 0.5, 0.75)}
-    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'>
-      <p className='text-white font-black text-[48px]'>"</p>
-      <div className="mt-1">
-    <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+  >
+    <p className='text-white font-black text-[48px]'>"</p>
+    <div className='mt-1'>
+      <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
 
-    <div className="mt-7 flex justify-between items-center gap-1">
-      <div className="flex-1 flex flex-col">
-        <p className='text-white font-medium text-[16px]'>
-          <span className='blue-text-gradient'>@</span> {name}
-        </p>
-        <p className='mt-1 text-secondary text-[12px]'>
-          {designation} of {company}
-        </p>
+      <div className='mt-7 flex justify-between items-center gap-1'>
+        <div className='flex-1 flex flex-col'>
+          <p className='text-white font-medium text-[16px]'>
+            <span className='blue-text-gradient'>@</span> {name}
+          </p>
+          <p className='mt-1 text-secondary text-[12px]'>
+            {designation} of {company}
+          </p>
+        </div>
+        <img
+          src={image}
+          alt={`feedback-by-${name}`}
+          className='w-10 h-10 rounded-full object-cover'
+        />
       </div>
-      <img
-        src={image}
-        alt={`feedback-by-${name}`}
-        className='w-10 h-10 rounded-full object-cover' />
     </div>
-      </div>
   </motion.div>
 )
 
 const Feedbacks = () => {
   return (
     <div className='mt-12 bg-black-100 rounded-[20px]'>
-      <div className={`${styles.padding} bg-tertiary rounded-2xl  min-h-[300px]`}>
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl  min-h-[300px]`}
+      >
         <motion.div variants={textVariant()}>
           <p className={styles.sectionSubText}>What Others Say</p>
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
@@ -43,11 +54,26 @@ const Feedbacks = () => {
       </div>
       <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
         {testimonials.map((testimonial, index) => (
-          <FeedbackCard
-            key={testimonial.name}
-            index={index}
-            {...testimonial} />
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
         ))}
+      </div>
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#contact'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
       </div>
     </div>
   )
